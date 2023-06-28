640c9a351d88177db36cfbfc3828ef63c0bad3d7
Koyo Nabeta <107094464+nabesanta@users.noreply.github.com>
2023-06-28 17:44:05 +0900

    Update ex1_qwiic_ICM20948.py

--- examples/ex1_qwiic_ICM20948.py
+++ examples/ex1_qwiic_ICM20948.py
@@ -37,7 +37,6 @@
 # SOFTWARE.
 #==================================================================================
 # Example 1
-#
 
 from __future__ import print_function
 import qwiic_icm20948
@@ -45,45 +44,35 @@ import time
 import sys
 
 def runExample():
+    print("\nSparkFun 9DoF ICM-20948 Sensor  Example 1\n")
+    IMU = qwiic_icm20948.QwiicIcm20948()
 
-	print("\nSparkFun 9DoF ICM-20948 Sensor  Example 1\n")
-	IMU = qwiic_icm20948.QwiicIcm20948()
+    if IMU.connected == False:
+        print("The Qwiic ICM20948 device isn't connected to the system. Please check your connection", file=sys.stderr)
+        return
 
-	if IMU.connected == False:
-		print("The Qwiic ICM20948 device isn't connected to the system. Please check your connection", \
-			file=sys.stderr)
-		return
-
-	IMU.begin()
-	file = open("accel_data.csv", "w")
-	while True:
-		for i in range(1000):
-			if IMU.dataReady():
-				IMU.getAgmt() # read all axis and temp from sensor, note this also updates all instance variables
-				print(\
-				 '{: 06d}'.format(IMU.axRaw)\
-				, '\t', '{: 06d}'.format(IMU.ayRaw)\
-				, '\t', '{: 06d}'.format(IMU.azRaw)\
-				, '\t', '{: 06d}'.format(IMU.gxRaw)\
-				, '\t', '{: 06d}'.format(IMU.gyRaw)\
-				, '\t', '{: 06d}'.format(IMU.gzRaw)\
-				, '\t', '{: 06d}'.format(IMU.mxRaw)\
-				, '\t', '{: 06d}'.format(IMU.myRaw)\
-				, '\t', '{: 06d}'.format(IMU.mzRaw)\
-				)
-			        file.write(str(IMU.axRaw) + "," + str(IMU.ayRaw) + "," + str(IMU.azRaw) + "," + str(IMU.gxRaw) + "," + str(IMU.gyRaw) + "," + str(IMU.gzRaw) + "\n")  
-			        time.sleep(0.02)
-			else:
-				print("Waiting for data")
-				time.sleep(0.5)
-		break
-	file.close()
+    IMU.begin()
+    file = open("accel_data.csv", "w")
+    while True:
+        for i in range(1000):
+            if IMU.dataReady():
+                IMU.getAgmt() # read all axis and temp from sensor, note this also updates all instance variables
+                print('{: 06d}'.format(IMU.axRaw), '\t', '{: 06d}'.format(IMU.ayRaw), '\t', '{: 06d}'.format(IMU.azRaw),
+                      '\t', '{: 06d}'.format(IMU.gxRaw), '\t', '{: 06d}'.format(IMU.gyRaw), '\t', '{: 06d}'.format(IMU.gzRaw),
+                      '\t', '{: 06d}'.format(IMU.mxRaw), '\t', '{: 06d}'.format(IMU.myRaw), '\t', '{: 06d}'.format(IMU.mzRaw))
+                file.write(str(IMU.axRaw) + "," + str(IMU.ayRaw) + "," + str(IMU.azRaw) + "," + str(IMU.gxRaw) + ","
+                           + str(IMU.gyRaw) + "," + str(IMU.gzRaw) + "\n")
+                time.sleep(0.02)
+            else:
+                print("Waiting for data")
+                time.sleep(0.5)
+        break
+    file.close()
 
 if __name__ == '__main__':
-	try:
-		runExample()
-	except (KeyboardInterrupt, SystemExit) as exErr:
-		print("\nEnding Example 1")
-		sys.exit(0)
-
+    try:
+        runExample()
+    except (KeyboardInterrupt, SystemExit) as exErr:
+        print("\nEnding Example 1")
+        sys.exit(0)
 
